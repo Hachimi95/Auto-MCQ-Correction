13711f66b259adb105de555133c9a7953149a754
Hachimi95 <yassinehachi001@gmail.com>
2022-07-04 21:17:02 +0200

    main and func files

---
+++ func.py
@@ -0,0 +1,100 @@
+import cv2
+import numpy as np
+
+def stackImages(imgArray,scale):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+
+    return ver
+
+
+def rectContours(contours) :
+    rectCon = []
+    # we have to filter out all contours to check if they have four corner points
+    for i in contours :
+        area= cv2.contourArea(i)
+        if area>50 :
+           peri = cv2.arcLength(i,True)           # find the total lengh of this contours
+           approx= cv2.approxPolyDP(i,0.02*peri,True) # approximate the contours to find who has 4 corners
+           # print("corner points" , len(approx))
+           if len(approx)==4 :
+               rectCon.append(i)
+    rectCon= sorted(rectCon,key=cv2.contourArea,reverse=True)
+
+    return rectCon
+
+
+def getCornerContours(cont) :
+    peri = cv2.arcLength(cont, True)
+    approx = cv2.approxPolyDP(cont, 0.02 * peri, True)
+    return  approx
+
+def reorder(points) :
+    points = points.reshape((4,2))
+    newPoints = np.zeros((4,1,2), np.int32)
+    #print(points)
+    add= points.sum(1)
+    newPoints[0] = points [np.argmin(add)]
+    newPoints[3] = points[np.argmax(add)]
+    newPoints[1] = points [3]
+    newPoints[2] = points[1]
+
+    return newPoints
+
+def spliBoxes(img) :
+    rows= np.vsplit(img,5)
+    boxes = []
+    for r in rows :
+        colms= np.hsplit(r,5)
+        for box in colms :
+            #cv2.imshow("sd", box)
+            boxes.append(box)
+
+
+    return  boxes
+
+def showAnswers(img,myIndex,grading,ans) :
+    secW = int(img.shape[1]/5)
+    secH = int(img.shape[0]/5)
+    for x in range(0,5) :
+        myAns = myIndex[x]
+        cX=(myAns*secW)+secW//2 # to know the centre of box area
+        cY=(x*secH)+secH//2
+        if grading[x]==1:
+            mycolor=(0,255,0)
+        else:
+            mycolor=(0,0,250)
+            cv2.circle(img, ( (ans[x] * secW) + secW // 2, (x * secH) + secH // 2), 20, (0,255,0), cv2.FILLED)
+
+        cv2.circle(img,(cX,cY),50,mycolor,cv2.FILLED)
+
+    return img
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,127 @@
+import cv2
+import numpy as np
+import func
+###########
+path = "qcm10.jpeg"
+widthImg=700
+heightImg=700
+ans = [ 1,2,0,1,4]
+webcamFeed = False
+###########
+cap = cv2.VideoCapture(0)
+cap.set(10,150)
+while True :
+    if webcamFeed : success ,img= cap.read()
+    else:  img = cv2.imread(path)
+    # resize our image
+    img=cv2.resize(img,(widthImg,heightImg))
+    imgContours = img.copy()
+    imgBiggestContours = img.copy()
+    imgfinal=img.copy()
+    # convert our image to Grayscale
+    imgGray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # add blur to our Grayscale image
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    #   detect our edges
+    imgCanny = cv2.Canny(imgBlur,10,50)
+
+    # find the image contours
+    # use the external method it will help us to find the outer edges
+    # the 3 argument to say we dont need any approximation
+    try :
+        contours,hierarchy = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        # to draw our contours
+        cv2.drawContours(imgContours,contours,-1,(0,250,0),10)
+        # find rectangles
+        rectCon = func.rectContours(contours)
+        biggestContours = func.getCornerContours(rectCon[0])
+        # reorder
+        func.reorder(biggestContours)
+        secondBigContours = func.getCornerContours(rectCon[1])
+
+
+        if biggestContours.size != 0 and secondBigContours.size != 0 :
+           cv2.drawContours(imgBiggestContours,biggestContours,-1,(0,255,0),20)
+           cv2.drawContours(imgBiggestContours,secondBigContours,-1,(255,0,0),20)
+
+           biggestContours = func.reorder(biggestContours)
+
+           p1 = np.float32(biggestContours)
+           p2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+           matrix = cv2.getPerspectiveTransform(p1, p2)
+           imgwarp = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+           secondBigContours = func.reorder(secondBigContours)
+
+           pg1 = np.float32(secondBigContours)
+           pg2 = np.float32([[0,0],[325,0],[0,150],[325,150]])
+           matrixg = cv2.getPerspectiveTransform(pg1, pg2)
+           imgwarpg = cv2.warpPerspective(img, matrixg, (325, 150 ))
+
+
+
+           imgwarpGray = cv2.cvtColor(imgwarp,cv2.COLOR_BGR2GRAY)
+           imgtresh= cv2.threshold(imgwarpGray,170,255,cv2.THRESH_BINARY_INV)[1]
+
+
+           boxes = func.spliBoxes(imgtresh)
+
+           mypixelval= np.zeros((5,5))
+           b =0
+           for i in range (0,5):
+               for j in range(0,5) :
+                  mypixelval[i][j] = cv2.countNonZero(boxes[b])
+                  b+=1
+
+           myIndex = []
+           for x in range(0,5) :
+                arry= mypixelval[x]
+                myIndexVal = np.where(arry == np.amax(arry))
+                #print(myIndex[0])
+                myIndex.append(myIndexVal[0][0])
+
+           grading=[]
+           for x in range(0,5) :
+               if ans[x]== myIndex[x] :
+                   grading.append(1)
+               else: grading.append(0)
+           #print(grading)
+           score = (sum(grading)/5) *100 # final grade
+           # display answer
+           imgAnsr=imgwarp.copy()
+           imgAnsr=func.showAnswers(imgAnsr, myIndex, grading, ans)
+
+
+           imgRawDrawing=np.zeros_like(imgwarp)
+           imgRawDrawing=func.showAnswers(imgRawDrawing, myIndex, grading, ans)
+
+           invmatrix = cv2.getPerspectiveTransform(p2, p1)
+           imginvwarp = cv2.warpPerspective(imgRawDrawing, invmatrix, (widthImg, heightImg))
+           # display  test grade on original image
+           imgRawGrade = np.zeros_like(imgwarpg)
+           cv2.putText(imgRawGrade,str(int(score))+"%",(60,100),cv2.FONT_HERSHEY_COMPLEX,3,(0,250,250),3)
+
+           invmatrixg = cv2.getPerspectiveTransform(pg2, pg1)
+           imginvwarpg = cv2.warpPerspective(imgRawGrade, invmatrixg , (widthImg, heightImg))
+
+           imgfinal= cv2.addWeighted(imgfinal,1,imginvwarp,1,0)
+           imgfinal= cv2.addWeighted(imgfinal,1,imginvwarpg,1,0)
+
+
+
+           imgBlank=np.zeros_like(img)
+           imgArray=([img,imgGray,imgBlur,imgCanny],[imgContours,imgBiggestContours,imgwarp,imgtresh],
+                     [imgAnsr,imgRawDrawing,imginvwarp,imgfinal])
+    except:
+        imgBlank=np.zeros_like(img)
+        imgArray=([imgBlank,imgBlank,imgBlank,imgBlank],[imgBlank,imgBlank,imgBlank,imgBlank],
+                     [imgBlank,imgBlank,imgBlank,imgBlank])
+
+    imgStacked=func.stackImages(imgArray,0.3)
+
+    cv2.imshow("Final image",imgfinal)
+    cv2.imshow("All image",imgStacked)
+    # ...
+    if cv2.waitKey(1) & 0xFF ==  ord("s") :
+        cv2.imwrite("FinalResult.jpg",imgfinal)
+        cv2.waitKey(300)
